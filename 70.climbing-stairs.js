cff11daf9d2363b95428d912147af460a51d4c2f
haixu.bo <haixu.bo@zhaopin.com.cn>
2019-11-01 17:55:33 +0800

    feat climb stair

---
+++ 70.climbing-stairs.js
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=70 lang=javascript
+ *
+ * [70] Climbing Stairs
+ */
+
+// @lc code=start
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n === 0) return 0
+  const dp = new Array(n).fill(0)
+
+  dp[1] = 1
+  dp[2] = 2
+  for (let i = 3; i <= n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2]
+  }
+  return dp[n]
+};
+// @lc code=end
+
+console.log('climbStairs', climbStairs(4))
